b023a49aa4d2129349db1f41b577481e7ad34b75
Petar Belchev <petar9004@gmail.com>
2023-02-12 19:51:00 +0200

    UI improvements

--- PersonalFinancer.Web/wwwroot/js/site.js
+++ PersonalFinancer.Web/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+	$('#datepicker').datepicker({
+		format: "dd.mm.yyyy",
+		todayBtn: "linked",
+		todayHighlight: true
+	});
+});
