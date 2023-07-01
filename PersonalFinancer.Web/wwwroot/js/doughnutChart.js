ebfb903df5848f687e01db248123c498516f7d96
Petar Belchev <petar9004@gmail.com>
2023-07-01 18:21:51 +0300

    Moved doughnut chart drawer to js file

---
+++ PersonalFinancer.Web/wwwroot/js/doughnutChart.js
@@ -0,0 +1,33 @@
+let canvas = document.querySelector('#doughnutChart');
+let chart;
+let isChartDrawed = false;
+
+function loadChart(currencyIndex) {
+	let categories = [];
+	let amounts = [];
+
+	for (let category of currencies[currencyIndex].ExpensesByCategories) {
+		categories.push(category.CategoryName);
+		amounts.push(category.ExpensesAmount.toFixed(2));
+	}
+
+	if (isChartDrawed) {
+		chart.destroy();
+	}
+
+	chart = new Chart(canvas, {
+		type: 'doughnut',
+		data: {
+			labels: categories,
+			datasets: [{
+				label: 'Amount',
+				data: amounts,
+				hoverOffset: 15
+			}]
+		}
+	});
+
+	isChartDrawed = true;
+};
+
+loadChart(0);
