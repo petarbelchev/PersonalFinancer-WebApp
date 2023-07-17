148c659018431d1fa489332a03dcda65dd34e073
Petar Belchev <petar9004@gmail.com>
2023-07-17 17:35:28 +0300

    The navpan now uses icons instead of text links.

--- PersonalFinancer.Web/wwwroot/js/hubs/notificationsHub.js
+++ PersonalFinancer.Web/wwwroot/js/hubs/notificationsHub.js
@@ -1,16 +1,16 @@
 let notificationsHub = new signalR.HubConnectionBuilder().withUrl('/notifications').build();
-let navpanMessagesBtn = document.querySelector('.navpanMessagesBtn');
+let navpanMessagesBtn = document.getElementById('navpanMessagesBtn');
 
 notificationsHub.on('ReceiveNotification', () => {
     if (navpanMessagesBtn.querySelector('.badge') == null) {
         navpanMessagesBtn.innerHTML += `
-		    <span class="badge text-bg-danger">New messages</span>
+			<span class="position-absolute top-40 start-80 translate-middle p-2 bg-danger rounded-circle"></span>
 	    `;
     }
 });
 
 notificationsHub.on('RemoveNotification', () => {
-    let notificationBadge = navpanMessagesBtn.querySelector('.badge');
+    let notificationBadge = navpanMessagesBtn.querySelector('span');
 
     if (notificationBadge != null) {
         navpanMessagesBtn.removeChild(notificationBadge);
